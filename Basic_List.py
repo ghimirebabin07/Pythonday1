7e8b1b12d1d06c659a5b7769fbe64c5eddc5e087
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2025-10-15 07:28:43 +0545

    Add Allbasic.py and Basic_List.py; update Practice Third.py

---
+++ Basic_List.py
@@ -0,0 +1,2 @@
+friends = ["Apple ", "Orange",5 ,45.06 ,False, "Aakash","Rohan"]
+print(friends[6])
--- Practice Third.py
+++ Practice Third.py
@@ -1,4 +1,3 @@
 #Write the python Program to display a user entered name followed by Good Afternoon using input() function 
 name = input("Enter the Name of the User :")
-print("Good AfterNoon,",name,"sir,How are you")
-print(f"Good Afternoon,{name}")
+print(name.find("Babin "))
